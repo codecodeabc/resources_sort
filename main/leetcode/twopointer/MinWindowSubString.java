90f7f3f96d5d91c427565e81ff94b5ed78302adf
chenpeipeng <chenpeipeng@henhaoji.com>
2021-03-26 20:56:00 +0800

    feat:添加未解题目

---
+++ main/leetcode/twopointer/MinWindowSubString.java
@@ -0,0 +1,23 @@
+package leetcode.twopointer;
+
+/**
+ * 滑动窗口
+ * 76. Minimum Window Substring (Hard)
+ * 3.6 练习 – 12/143 –
+ * 题目描述
+ * 给定两个字符串 S 和 T，求 S 中包含 T 所有字符的最短连续子字符串的长度，同时要求时间
+ * 复杂度不得超过 O(n)。
+ * 输入输出样例
+ * 输入是两个字符串 S 和 T，输出是一个 S 字符串的子串。
+ * Input: S = "ADOBECODEBANC", T = "ABC"
+ * Output: "BANC"
+ */
+public class MinWindowSubString {
+    public static String minWindow(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        int i = 0;
+        int j = 0;
+        return null;
+    }
+}
